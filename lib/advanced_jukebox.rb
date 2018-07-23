e9e0490eaf95f2c5b73e24011e4da1a309d524b3
Felix Chan <felixchan29@gmail.com>
2018-07-23 04:03:03 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,6 +3,7 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
+<<<<<<< HEAD
  "Go Go GO" => '/home/chanfe/jukebox-cli-dumbo-web-080618/audio/Emerald-Park/01.mp3',
  "LiberTeens" => '/home/chanfe/jukebox-cli-dumbo-web-080618/jukebox-cli/audio/Emerald-Park/02.mp3',
  "Hamburg" =>  '/home/chanfe/jukebox-cli-dumbo-web-080618/jukebox-cli/audio/Emerald-Park/03.mp3',
@@ -10,6 +11,15 @@ my_songs = {
  "Wolf" => '/home/chanfe/jukebox-cli-dumbo-web-080618/jukebox-cli/audio/Emerald-Park/05.mp3',
  "Blue" => '/home/chanfe/jukebox-cli-dumbo-web-080618/jukebox-cli/audio/Emerald-Park/06.mp3',
  "Graduation Failed" => '/home/chanfe/jukebox-cli-dumbo-web-080618/jukebox-cli/audio/Emerald-Park/07.mp3'
+=======
+ "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
+ "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
+ "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
+ "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
+ "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+>>>>>>> 14f5cd44b08dbee9e99554f1e3395ca7864ca5df
  }
 
 def help
@@ -27,8 +37,13 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+<<<<<<< HEAD
   my_songs.collect do |key, value|
     puts "#{key}"
+=======
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+>>>>>>> 14f5cd44b08dbee9e99554f1e3395ca7864ca5df
   end
   
 end
@@ -44,8 +59,15 @@ def play(my_songs)
   #get the file path of the song by looking it up in the my_songs hash
   puts "Please enter a song name or number:"
   user_response = gets.chomp
+<<<<<<< HEAD
   if my_songs.include?(user_response)
     system "open #{my_songs[user_response]}"
+=======
+  if songs.include?(user_response)
+    puts "Playing #{user_response}"
+  elsif user_response.to_i <= songs.length && user_response.to_i > 0
+    puts "Playing #{songs[(user_response.to_i)-1]}"
+>>>>>>> 14f5cd44b08dbee9e99554f1e3395ca7864ca5df
   else
     puts "Invalid input, please try again"
   end
